02fd9d20ff4720626a316117fca6f7a96c12bf34
Jagannath Varma Kalidindi <50195473+jagannathvarma@users.noreply.github.com>
2021-05-07 11:33:27 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/Jagannath.jpg'),
+              ),
+              Text(
+                'Jagannath Varma Kalidindi',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 25.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.teal.shade100,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
